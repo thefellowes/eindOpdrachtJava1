916aaec96858f4b709d5679f81361b5e0f897295
thefellowes <marcogeertsma@gmail.com>
2017-12-17 20:24:20 +0100

    Started linking new table to observable table

--- src/sample/ListViewController.java
+++ src/sample/ListViewController.java
@@ -1,10 +1,15 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.util.Observable;
 
 public class ListViewController extends ViewController {
@@ -23,4 +28,25 @@ public class ListViewController extends ViewController {
         return;
     }
 
+    private final ObservableList<Movie> movieDataObservableList
+            = FXCollections.observableArrayList();
+    public TableView movieTable;
+    public Button getMoviesButton;
+
+    public void getMoviesButton() {
+        //getMoviesFromController();
+    }
+
+    /*private void getMoviesFromController() {
+        ListViewController controller = new ListViewController();
+        try {
+            movieDataObservableList.setAll(controller.getMovies());
+        } catch (IOException e) {
+            System.out.println("Failed to connect to the controller");
+        }
+    }*/
+
+    public ObservableList<Movie> getMovieDataObservableList() {
+        return movieDataObservableList;
+    }
 }
