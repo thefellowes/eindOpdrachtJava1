acbd8d92c9a20b7a489557684dd52eeb775b9bb9
Peter van der Velde <petervandervelde2@gmail.com>
2017-12-17 17:22:19 +0100

    tmp

--- src/sample/Movie.java
+++ src/sample/Movie.java
@@ -1,16 +1,26 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
     private String name;
     private int productionYear;
     private String countryOfOrigin;
     private int budget;
 
+    public List<Movie> movies = new ArrayList<>();
+
     public Movie(String name, int productionYear, String countryOfOrigin, int budget) {
         this.name = name;
         this.productionYear = productionYear;
         this.countryOfOrigin = countryOfOrigin;
         this.budget = budget;
+        movies.add(new Movie(name, productionYear, countryOfOrigin, budget));
+    }
+
+    public List<Movie> getMovies(){
+        return movies;
     }
 
     public String getName() {
