72c4580f36d68960e229080bc627a74b8c4901aa
Peter van der Velde <petervandervelde2@gmail.com>
2017-12-18 16:43:55 +0100

    added bars

--- src/sample/GraphViewController.java
+++ src/sample/GraphViewController.java
@@ -4,11 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
-import java.util.Arrays;
+
+import java.util.ArrayList;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.Initializable;
+import javafx.scene.chart.XYChart;
 
+import java.util.List;
 import java.util.Observable;
 
 public class GraphViewController extends ViewController {
@@ -19,8 +22,7 @@ public class GraphViewController extends ViewController {
     private BarChart barChart;
 
     public GraphViewController() {
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
+
     }
 
     public void displayGraphs() {
@@ -32,7 +34,49 @@ public class GraphViewController extends ViewController {
     public void update(Observable o, Object model) {
         this.model = (Model) model;
         System.out.println("updated GraphViewController");
-        return;
+
+        List<Movie> movies = ((Model) model).getMovies();
+
+        // Create a simple struct
+        class countryStruct {
+            public String name;
+            public int amount;
+        }
+
+        List<countryStruct> countries = new ArrayList<countryStruct>();
+
+        // Generate Data
+        for (Movie movie : movies) {
+            boolean alreadyInList = false;
+
+            for (countryStruct countryStruct : countries) {
+                if (movie.getCountryOfOrigin().equals(countryStruct.name)){
+                    countryStruct.amount += 1;
+                    alreadyInList = true;
+                }
+            }
+            if (!alreadyInList) {
+                countryStruct countryToBeAdded = new countryStruct();
+                countryToBeAdded.name = movie.getCountryOfOrigin();
+                countryToBeAdded.amount = 1;
+                countries.add(countryToBeAdded);
+            }
+        }
+
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        ObservableList<XYChart.Series<String, Number>> barChartData = FXCollections.observableArrayList();
+
+        final BarChart.Series<String, Number> series =  new BarChart.Series<String, Number>();
+        for (countryStruct country : countries) {
+            series.getData().add(new XYChart.Data<String, Number>(country.name, country.amount));
+        }
+
+        // add data to chart
+        barChartData.add(series);
+        barChart.setData(barChartData);
+
     }
 
 }
