f08babfb65ba258e879413ea2aff49cd4043e70a
thefellowes <marcogeertsma@gmail.com>
2017-12-18 16:42:47 +0100

    More listView extraveganza

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,8 +14,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    Stage window;
-    TableView<Movie> table;
+    //Stage window;
+    //TableView<Movie> table;
 
     private int width = 500;
     private int height = 375;
@@ -27,41 +27,41 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         Model model = new Model();
-        window = primaryStage;
-        window.setTitle("List of movies");
-
-        //Set the column names
-        //Name
-        TableColumn<Movie, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setMinWidth(200);
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
-        //Production Year
-        TableColumn<Movie, Integer> yearColumn = new TableColumn<>("Year");
-        yearColumn.setMinWidth(200);
-        yearColumn.setCellValueFactory(new PropertyValueFactory<>("productionYear"));
-
-        //Country of origin
-        TableColumn<Movie, String> originColumn = new TableColumn<>("Origin");
-        originColumn.setMinWidth(200);
-        originColumn.setCellValueFactory(new PropertyValueFactory<>("countryOfOrigin"));
-
-        //Budget
-        TableColumn<Movie, Double> budgetColumn = new TableColumn<>("Budget");
-        budgetColumn.setMinWidth(200);
-        budgetColumn.setCellValueFactory(new PropertyValueFactory<>("budget"));
-
-        //Setting the table
-        table = new TableView<>();
-        table.setItems(getMovieList());
-        table.getColumns().addAll(nameColumn, yearColumn, originColumn, budgetColumn);
-
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(table);
-
-        Scene scene = new Scene(vBox);
-        window.setScene(scene);
-        window.show();
+//        window = primaryStage;
+//        window.setTitle("List of movies");
+//
+//        //Set the column names
+//        //Name
+//        TableColumn<Movie, String> nameColumn = new TableColumn<>("Name");
+//        nameColumn.setMinWidth(200);
+//        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+//
+//        //Production Year
+//        TableColumn<Movie, Integer> yearColumn = new TableColumn<>("Year");
+//        yearColumn.setMinWidth(200);
+//        yearColumn.setCellValueFactory(new PropertyValueFactory<>("productionYear"));
+//
+//        //Country of origin
+//        TableColumn<Movie, String> originColumn = new TableColumn<>("Origin");
+//        originColumn.setMinWidth(200);
+//        originColumn.setCellValueFactory(new PropertyValueFactory<>("countryOfOrigin"));
+//
+//        //Budget
+//        TableColumn<Movie, Double> budgetColumn = new TableColumn<>("Budget");
+//        budgetColumn.setMinWidth(200);
+//        budgetColumn.setCellValueFactory(new PropertyValueFactory<>("budget"));
+//
+//        //Setting the table
+//        table = new TableView<>();
+//        table.setItems(getMovieList());
+//        table.getColumns().addAll(nameColumn, yearColumn, originColumn, budgetColumn);
+//
+//        VBox vBox = new VBox();
+//        vBox.getChildren().addAll(table);
+//
+//        Scene scene = new Scene(vBox);
+//        window.setScene(scene);
+//        window.show();
 
 
         FXMLLoader fxmlLoader = new FXMLLoader();
